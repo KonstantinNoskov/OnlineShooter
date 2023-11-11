d973208ca73bd1a4f0ca108fb6e091216c8a7081
Константин <kosinosweb@playrix.com>
2023-11-11 18:02:10 +0500

    Animation Instance for Belica
    1. Created animation instance
    2. Added few basic animation for running and jumping.
    
    ONLINE TESTED: SUCCESSFUL

---
+++ Source/OnlineShooter/Private/Characters/OnlineShooterAnimInstance.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/OnlineShooterAnimInstance.h"
+
+#include "Characters/OnlineShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UOnlineShooterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// Get the reference to a player character 
+	OnlineShooterCharacter = Cast<AOnlineShooterCharacter>(TryGetPawnOwner());
+	
+}
+
+void UOnlineShooterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	/* ANIMATION PROPERTIES
+	 * ==============================================================================================================
+	 * 
+	 * Following properties will determine which character animation is gonna be played
+	 * Properties updates every "animation tick"
+	 *
+	 * @var Speed - horizontal speed
+	 * @var bIsInAir - whether character is falling, jumping, flying etc.
+	 * @var bIsAccelerating - it's NOT the rate of change of velocity in physical understanding. It's just determines
+	 *		whether player pressed keyboard key to move.
+	 *		
+	 *
+	 * ==============================================================================================================
+	 */
+	
+	// if player reference is nov valid, try to get player reference
+	if (!OnlineShooterCharacter) { OnlineShooterCharacter = Cast<AOnlineShooterCharacter>(TryGetPawnOwner()); }
+
+	// if player reference still is null, return
+	if(!OnlineShooterCharacter) { return; }
+
+	// Speed (horizontal)
+	FVector Velocity = OnlineShooterCharacter->GetVelocity();
+	Speed = Velocity.Size2D();
+
+	bIsInAir = OnlineShooterCharacter->GetCharacterMovement()->IsFalling();
+	
+	bIsAccelerating = OnlineShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0 ? true : false;
+}
---
+++ Source/OnlineShooter/Public/Characters/OnlineShooterAnimInstance.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "OnlineShooterAnimInstance.generated.h"
+
+class AOnlineShooterCharacter;
+/**
+ * 
+ */
+UCLASS()
+class ONLINESHOOTER_API UOnlineShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+
+	UPROPERTY(BlueprintReadOnly, Category = "Character", meta = (AllowPrivateAccess = "true"))
+	AOnlineShooterCharacter* OnlineShooterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+	
+};
